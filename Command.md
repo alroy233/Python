9b04681409d2002cc4390e89bc46af823d031c81
Kasaken <alroy233@yahoo.com>
2019-08-08 18:24:11 +0800

    Update 2019年8月8日 18:24:08

---
+++ Command.md
@@ -0,0 +1,12 @@
+## 1、内置函数BIF-built in function
+```python
+dir(__builtins__)
+help(BIF)
+```
+
+## 2、转义，原始字符串
+```python
+print('let\'s go')
+print(r"let's go C:\\NP")
+```
+
--- Exercise/if.py
+++ Exercise/if.py
@@ -1,6 +1,22 @@
-num = input("input a number")
-guess = int(num)
-if guess == 3:
-    print("right")
+import random
+ran = random.randint(1,500)
+print (ran)
+guess = int(input("input a number"))
+i=int(5)
+while(guess != ran):
+    if(i==1):
+        print("fail")
+        break
+    else:
+        if(guess>ran):
+            i=i-1
+            print("too big,you have ["+str(i)+"] times left to try")
+            guess = int(input("input a number again"))
+        else:
+            i=i-1
+            print("too small,you have ["+str(i)+"] times left to try")
+            guess = int(input("input a number again"))
+if(i != 1 or guess == ran):
+    print("✔✔✔猜对了✔✔✔")
 else:
-    print("wrong")
+    print("you have no chance")
