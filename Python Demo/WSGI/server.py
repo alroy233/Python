b5d8ae7030ad7cfcd203660e732a02298e99967b
alroy233 <824559791@qq.com>
2020-08-07 00:59:58 +0800

    add

---
+++ Python Demo/WSGI/server.py
@@ -0,0 +1,16 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================ RESTART: Shell ================================
+>>> 
+# server.py
+# 从wsgiref模块导入:
+from wsgiref.simple_server import make_server
+# 导入我们自己编写的application函数:
+from hello import application
+
+# 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+# 开始监听HTTP请求:
+httpd.serve_forever()
